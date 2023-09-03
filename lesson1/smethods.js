9a4c28c7f2b03d344f9b1f9225fce4898a06136c
alyorjon <alyorjon>
2023-09-03 11:13:57 +0500

    leson

---
+++ lesson1/smethods.js
@@ -0,0 +1,14 @@
+"use strict";
+const user='Aisha';
+console.log(user.split('i'))
+// ['a'],['sha']
+console.log(user.length)
+// 5
+console.log(user[0])
+// A
+console.log(user.replace('i','o'))
+// aosha
+console.log(user.toUpperCase())
+// AISHA
+console.log(user.toLowerCase())
+// aisha
